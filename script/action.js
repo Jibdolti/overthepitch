47297e8c499d1c4aa89861511ccc275213cad6a2
Jibdolti <154307791+Jibdolti@users.noreply.github.com>
2024-09-20 12:16:50 +0900

    update

--- script/action.js
+++ script/action.js
@@ -33,3 +33,19 @@ $(document).ready(function() {
       updateIndicator(sectionIndex); // 인디케이터 위치 업데이트
     });
   });
+
+  // 확대/축소 방지 코드
+  document.addEventListener('wheel', function(event) {
+    if (event.ctrlKey) {
+        event.preventDefault();
+    }
+}, { passive: false });
+
+document.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && (event.key === '+' || event.key === '-' || event.key === '0')) {
+        event.preventDefault();
+    }
+});
+
+// 페이지 로드 시 기본 배율로 설정
+document.body.style.zoom = "100%"; // 일부 브라우저에서 배율을 강제 설정
